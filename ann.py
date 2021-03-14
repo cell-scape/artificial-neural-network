abe38ee6251d47a551a9a846ec5b9715321faa4f
Bradley Dowling <brad.dowling@protonmail.com>
2021-03-14 00:38:02 -0500

    initial commit

---
+++ ann.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import sys
+from argparse import ArgumentParser
+from random import uniform
+from math import exp
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mnist import MNIST
+
+if __name__ == '__main__':
+    sys.exit(0)
+
+
+
+
+
